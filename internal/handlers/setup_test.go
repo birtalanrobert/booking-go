4b7aafc7f80dbb8ec6c79eb0476f576f09fb7073
Birtalan Robert <robert.birtalan@fortech.ro>
2024-09-06 12:53:01 +0300

    update tests

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -47,6 +47,12 @@ func TestMain(m *testing.M) {
 
 	app.Session = session
 
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
+	defer close(mailChan)
+
+	listenForMail()
+
 	tc, err := CreateTestTemplateCache()
 	
 	if err != nil {
@@ -148,4 +154,12 @@ func CreateTestTemplateCache() (map[string]*template.Template, error) {
 	}
 
 	return myCache, nil
+}
+
+func listenForMail() {
+	go func() {
+		for {
+			<- app.MailChan
+		}
+	}()
 }
