15f149129edb3ecfd87507d2407140c23b629973
Birtalan Robert <robert.birtalan@fortech.ro>
2024-09-10 11:01:59 +0300

    use flags instead of hardcoded config values

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go build -o bookings cmd/web/*.go && ./bookings
+go build -o bookings cmd/web/*.go && ./bookings -dbname=bookings -dbuser=robertbirtalan -cache=false -production=false
