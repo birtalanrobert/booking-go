5f485b6b7a8ee684068e597d2ca0a9d89af92e3e
Birtalan Robert <robert.birtalan@fortech.ro>
2024-09-10 11:13:31 +0300

    migration for the admin user

---
+++ migrations/20240910080242_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO users ("first_name","last_name","email","password","acess_level","created_at","updated_at")
+VALUES (E'Robert',E'Birtalan',E'birtalanrobert@gmail.com',E'$2a$10$PjlUWXcA2NsU5OR1RU0sP.Ef.8MTnUlGZp10kvvTpgxEGWBU5Nvtm',E'3',E'2024-09-10 00:00:00',E'2024-09-10 00:00:00')
